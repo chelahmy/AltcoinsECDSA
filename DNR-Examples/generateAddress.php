2d6d0b52fd8a0cbd85158ae2528ec090e53ec267
Abdullah Daud <chelahmy@gmail.com>
2017-10-17 16:52:29 +0800

    Renamed Bitcoin to Altcoin

--- DNR-Examples/generateAddress.php
+++ DNR-Examples/generateAddress.php
@@ -1,16 +1,16 @@
 <?php
 
-require_once '../src/BitcoinPHP/BitcoinECDSA/BitcoinECDSA.php';
+require_once '../src/AltcoinsECDSA.php';
 
-use BitcoinPHP\BitcoinECDSA\BitcoinECDSA;
+use AltcoinsECDSA\AltcoinsECDSA;
 
-$bitcoinECDSA = new BitcoinECDSA('DNR');
-$bitcoinECDSA->generateRandomPrivateKey(); //generate new random private key
-$address = $bitcoinECDSA->getAddress(); //compressed Bitcoin address
+$altcoinECDSA = new AltcoinsECDSA('DNR');
+$altcoinECDSA->generateRandomPrivateKey(); //generate new random private key
+$address = $altcoinECDSA->getAddress(); //compressed Altcoin address
 echo "Address: " . $address . PHP_EOL;
 
 //Validate an address (Verify the checksum)
-if($bitcoinECDSA->validateAddress($address)) {
+if($altcoinECDSA->validateAddress($address)) {
     echo "The address is valid" . PHP_EOL;
 } else {
     echo "The address is invalid" . PHP_EOL;
